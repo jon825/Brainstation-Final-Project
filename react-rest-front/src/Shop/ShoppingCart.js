ac814d3b996ea876f090efc584a2b829d042d3a3
jon825 <kui825@gmail.com>
2016-12-01 17:35:16 -0500

    update

--- react-rest-front/src/Shop/ShoppingCart.js
+++ react-rest-front/src/Shop/ShoppingCart.js
@@ -2,16 +2,17 @@ import React from 'react';
 
 class ShoppingCart extends React.Component{
     constructor(){
+        let price = this.props.totalprice
         super();
         this.state={
-           
+           totalprice:price
         }
 
     }
 
     
     render(){
-        console.log(this.props.totalprice)
+        console.log(this.state.totalprice)
         return(
             <div>
             Shopping Cart
