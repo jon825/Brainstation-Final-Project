b072054e132c5e8f0970425593e96c1b36f97ca9
jon825 <kui825@gmail.com>
2016-12-02 10:42:22 -0500

    shopping cart update

--- react-rest-front/src/Shop/ShoppingCart.js
+++ react-rest-front/src/Shop/ShoppingCart.js
@@ -24,7 +24,7 @@ class ShoppingCart extends React.Component{
                             
                             </li>
                             </ul>
-                            </div>
+                            </div> 
             </div>
 
 
