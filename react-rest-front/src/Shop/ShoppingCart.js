2b01a424a9ec92bad7814ced1fb0d6a52e4cf8f1
jon825 <kui825@gmail.com>
2016-12-02 10:43:54 -0500

    shopping car update

--- react-rest-front/src/Shop/ShoppingCart.js
+++ react-rest-front/src/Shop/ShoppingCart.js
@@ -1,30 +1,30 @@
 import React from 'react';
 
-class ShoppingCart extends React.Component{
-    constructor(){
+class ShoppingCart extends React.Component {
+    constructor() {
         // let price = this.props.totalprice
         super();
-        this.state={
-     
+        this.state = {
+
         }
 
     }
 
-    
-    render(){
+
+    render() {
         console.log(this.props.totalprice)
         console.log(this.props.quantity)
-        return(
+        return (
             <div className="row">
-                <div> className="col-sm-6 col-md-6">
+                <div className="col-sm-6 col-md-6">
                     <ul className="list-group">
                         <li className="list-group-item">
                             <strong>Title</strong>
                             <span className="label label-success">${this.props.totalprice}</span>
-                            
-                            </li>
-                            </ul>
-                            </div> 
+
+                        </li>
+                    </ul>
+                </div>
             </div>
 
 
